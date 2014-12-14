b0a20084c6847960abe9dc82ef3c606a097d3e01
Momday <md@kaneoh.org>
2014-12-15 02:31:37 +0800

    Rename files to kube.import.less and colors.import.less

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,14 +1,15 @@
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
+  Template.layout.helpers({
     counter: function () {
       return Session.get("counter");
     }
   });
 
-  Template.hello.events({
+  Template.layout.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
